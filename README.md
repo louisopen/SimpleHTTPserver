739092c8de9f71dc119b4c38e8b64daec12cd000
Louis Huang <louisopen@gmail.com>
2019-03-29 16:54:39 +0800

    Fisrt code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# SimpleHTTPserver
+## SimpleHTTPserver
+Start VSCode
+Under command line 
+SSH pi@192.168.0.12    # need your password of the pi account.
+rmate –p 52698 simple_webserver.py  # edit the program (soucer code)
+Ctrl+S  # save the soucer code to remote raspberry pi
+python3 ./simple_webserver.py   # Run http server
 
+Start Edge
+192.168.0.12:8000
---
+++ simple_webserver.py
@@ -0,0 +1,85 @@
+"""
+python3 simple_webserver.py      # Run command to handle multiple GET request in the HW platform.
+example:
+http://192.168.0.114:8000/       # Display the webpage without LED status
+http://192.168.0.114:8000/on     # Turn on the LED and display LED is On
+http://192.168.0.114:8000/off    # Turn off the LED and display LED is off
+"""
+import RPi.GPIO as GPIO          # Check it in your windows or Raspbian platform
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer      # must be run python3 -m http.server   
+
+
+class MytestHTTPServer(BaseHTTPRequestHandler):
+    """ A special implementation of BaseHTTPRequestHander for reading data from
+        and control GPIO of a Raspberry Pi
+    """
+
+    def do_HEAD(self):
+        """ do_HEAD() can be tested use curl command 
+            'curl -I http://server-ip-address:port' 
+        """
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def _redirect(self, path):
+        self.send_response(303)
+        self.send_header('Content-type', 'text/html')
+        self.send_header('Location', path)
+        self.end_headers()
+
+    def do_GET(self):
+        """ do_GET() can be tested using curl command 
+            'curl http://server-ip-address:port' 
+        """
+        html = '''
+            <html>
+            <body style="width:960px; margin: 20px auto;">
+            <h1>Welcome to my Raspberry Pi</h1>
+            <p>Current GPU temperature is {}</p>
+            <form action="/" method="POST">
+                Turn LED :
+                <input type="submit" name="submit" value="On">
+                <input type="submit" name="submit" value="Off">
+            </form>
+            </body>
+            </html>
+        '''
+        temp = os.popen("/opt/vc/bin/vcgencmd measure_temp").read()
+        self.do_HEAD()
+        self.wfile.write(html.format(temp[5:]).encode("utf-8"))
+
+    def do_POST(self):
+        """ do_POST() can be tested using curl command 
+            'curl -d "submit=On" http://server-ip-address:port' 
+        """
+        content_length = int(self.headers['Content-Length'])    # Get the size of data
+        post_data = self.rfile.read(content_length).decode("utf-8")   # Get the data
+        post_data = post_data.split("=")[1]    # Only keep the value
+        
+        # GPIO setup
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(18,GPIO.OUT)
+
+        if post_data == 'On':
+            GPIO.output(18, GPIO.HIGH)
+        else:
+            GPIO.output(18, GPIO.LOW)
+        print("LED is {}".format(post_data))
+        self._redirect('/')      # Redirect back to the root url
+
+def run():
+    #host_name = '10.132.10.25'   # your Raspberry Pi IP address
+    host_name = ''
+    #host_name = 'localhost'      # Not working now
+    host_port = 8000             # print('starting server, port', host_port)
+    # Server settings
+    server_address = (host_name, host_port) 
+    print('running server...', server_address)
+    httpd = HTTPServer(server_address, MytestHTTPServer)
+    httpd.serve_forever()
+
+if __name__ == '__main__': 
+    run()

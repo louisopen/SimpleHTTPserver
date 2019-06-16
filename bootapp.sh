f2d06702287256220449a76d9be838811272031b
Louis Huang <louisopen@gmail.com>
2019-06-16 22:50:52 +0800

    update bash

--- bootapp.sh
+++ bootapp.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
-sudo python3 /home/pi/simple_webserver.py
+#!/bin/sh 
+#This bash called by /home/pi/.config/autostart/autoboot.desktop
+jump_dir=/home/pi/SimpleHTTPserver
+#tr -d "\r" < oldname.sh > newname.sh   #if you can't cd to .... just do this command.
+cd $jump_dir
+pwd
+sudo python3 $jump_dir/simple_webserver.py
+
+exit 0
